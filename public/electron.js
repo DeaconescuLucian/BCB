33ce50a121540e05ed66a30a5eea62a468361f0a
DeaconescuLucian <deaconesculucian18@stud.ase.ro>
2024-08-07 22:12:56 +0300

    added Tray Icon

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,8 @@
 const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Tray = electron.Tray;
+const Menu = electron.Menu;
 
 const path = require("path");
 const isDev = require("electron-is-dev");
@@ -13,14 +15,15 @@ require('electron-reload')(__dirname, {
 });
 
 let mainWindow;
+let tray;
 remoteMain.initialize();
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     minWidth: 900,
     minHeight: 680,
-    width: 900, 
-    height: 680, 
+    width: 900,
+    height: 680,
     frame: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
@@ -38,19 +41,52 @@ function createWindow() {
   mainWindow.setMenuBarVisibility(false);
   remoteMain.enable(mainWindow.webContents);
   mainWindow.webContents.openDevTools();
-  mainWindow.on("closed", () => {process.exit(1);});
+  mainWindow.on("closed", () => { mainWindow = null; });
 }
 
-app.on("ready", createWindow);
+function createTray() {
+  const iconPath = path.join(__dirname, 'solana.png');
+  tray = new Tray(iconPath);
 
-app.on("window-all-closed", () => {
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Show App',
+      click: () => {
+        if (mainWindow === null) {
+          createWindow();
+        } else {
+          mainWindow.show();
+        }
+      },
+    },
+    {
+      label: 'Quit',
+      click: () => {
+        process.exit(1);
+      },
+    },
+  ]);
+
+  tray.setContextMenu(contextMenu);
+  tray.setToolTip('Blockchain Busters');
+}
+
+app.on("ready", () => {
+  createTray();
+  createWindow();
+});
+
+app.on("window-all-closed", (event) => {
   if (process.platform !== "darwin") {
-    process.exit(1);
+    event.preventDefault();
+    mainWindow.hide();
   }
 });
 
 app.on("activate", () => {
   if (mainWindow === null) {
     createWindow();
+  } else {
+    mainWindow.show();
   }
 });
